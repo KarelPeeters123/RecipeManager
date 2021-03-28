55dbd39bafd45c3a270cee455c225dc790b69cdb
Karel <Karel.Peeters@ciminko.lu>
2021-03-28 14:02:10 +1300

    created tableView with dummy data

--- RecipeManager/RecipeManager/ViewController.swift
+++ RecipeManager/RecipeManager/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var recipeTable: UITableView!
+    
+    var recipes = [
+        "Pancakes",
+        "Brownies",
+        "Cinnamon Rolls"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        recipeTable.delegate = self
+        recipeTable.dataSource = self
+        
         // Do any additional setup after loading the view.
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("clicked row");
+    }
+}
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recipes.count;
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell1", for: indexPath)
+        cell.textLabel?.text = recipes[indexPath.row];
+        return cell
+    }
+}
